bc217038fbc892d46454e12831014ca88d0c6c92
msfvf99 <dkaragadyan@bk.ru>
2024-06-18 15:27:57 +0300

    Оторбажение списком заказов в корзине

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Store.Memory
@@ -19,6 +20,15 @@ namespace Store.Memory
                 "Known as the bible of C, this classic bestseller introduces the",
                 14.98m),
         };
+
+        public Book[] GetAllByIds(IEnumerable<int> bookIds)
+        {
+            var foundBooks = from book in books
+                             join bookId in bookIds on book.Id equals bookId
+                             select book;
+
+            return foundBooks.ToArray();
+        }
 
         public Book[] GetAllByIsbn(string isbn)
         {
